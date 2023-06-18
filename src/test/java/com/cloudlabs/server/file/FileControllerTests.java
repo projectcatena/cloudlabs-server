863b44fc7a745d5dbda657a526da121f175ca75c
Kai <kaizhetan@yahoo.com>
2023-06-18 17:19:23 +0800

    test: Add tests for start build

--- src/test/java/com/cloudlabs/server/file/FileControllerTests.java
+++ src/test/java/com/cloudlabs/server/file/FileControllerTests.java
@@ -1,9 +1,14 @@
 package com.cloudlabs.server.file;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.charset.Charset;
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -15,16 +20,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-// @ExtendWith(MockitoExtension.class)
 public class FileControllerTests {
 
     @Autowired
     protected MockMvc mockMvc;
 
     private ObjectMapper objectMapper = new ObjectMapper();
-
-    // @Autowired
-    // protected FileService fileService;
 
     @Mock
     private FileService fileService;
@@ -84,4 +85,53 @@ public class FileControllerTests {
                         """))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
     } 
+
+    /**
+     * Expensive test, as it will launch an actual build on GCP and return its build ID.
+     * Solution: Cancel build using the returned build ID before completion.
+     * 
+     * @throws Exception
+     */
+    @Test
+    void startBuild_whenCorrectParametersGiven() throws Exception {
+        FileDTO file = new FileDTO();
+        // Test file must already exist in GCP bucket
+        file.setObjectName("Windows_Server_2019-disk1.vmdk");
+        file.setImageName("windows-server-2019");
+
+        String jsonString = objectMapper.writeValueAsString(file);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/storage/start")
+                .contentType(MediaType.APPLICATION_JSON).content(jsonString))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.objectName").value(file.getObjectName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.imageName").value(file.getImageName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.buildStatus").value("QUEUED"));
+    } 
+
+    @Test
+    void failStartBuild_whenParametersNotGiven() throws Exception {
+        FileDTO file = new FileDTO();
+
+        String jsonString = objectMapper.writeValueAsString(file);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/storage/start")
+                .contentType(MediaType.APPLICATION_JSON).content(jsonString))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void failStartBuild_whenFileDoesNotExistInBucket() throws Exception {
+        // Generate random string
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-8"));
+
+        FileDTO file = new FileDTO();
+        file.setObjectName(generatedString + ".vmdk");
+        file.setImageName("test-file-does-not-exist");
+
+        String jsonString = objectMapper.writeValueAsString(file);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/storage/start")
+                .contentType(MediaType.APPLICATION_JSON).content(jsonString))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
 }
