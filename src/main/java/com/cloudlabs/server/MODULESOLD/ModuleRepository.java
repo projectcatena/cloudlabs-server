6d6dd15673ec46756889016e1b2faf0742b834f6
Boon Chong Chuah <s10219129@connect.np.edu.sg>
2023-07-08 22:16:29 +0800

    fix: case sensitive folder1

---
+++ src/main/java/com/cloudlabs/server/MODULESOLD/ModuleRepository.java
@@ -0,0 +1,15 @@
+package com.cloudlabs.server.MODULESOLD;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ModuleRepository extends JpaRepository<Module, Long> {
+    Module findByModuleId(Long moduleId);
+}
