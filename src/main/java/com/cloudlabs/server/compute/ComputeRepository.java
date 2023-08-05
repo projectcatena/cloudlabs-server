ee2ef5e0274f10b39d90d96ad2e78e1e834f708c
DancinParrot <kaizhetan@yahoo.com>
2023-08-05 12:46:56 +0800

    feat(compute): list compute by module and user

--- src/main/java/com/cloudlabs/server/compute/ComputeRepository.java
+++ src/main/java/com/cloudlabs/server/compute/ComputeRepository.java
@@ -6,17 +6,16 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComputeRepository extends JpaRepository<Compute, Long> {
-    Optional<Compute> findByInstanceName(String instanceName);
+  Optional<Compute> findByInstanceName(String instanceName);
 
-    List<Compute> findByUsers_Email(String email);
+  List<Compute> findByUsers_Email(String email);
 
-    List<Compute> findByUsers_EmailAndModuleId(String email, Long moduleId);
+  List<Compute> findByUsers_EmailAndModuleId(String email, Long moduleId);
 
-    Optional<Compute> findByUsers_EmailAndInstanceName(String email,
-            String instanceName);
+  Optional<Compute> findByUsers_EmailAndInstanceName(String email,
+                                                     String instanceName);
 
-    // Will issue select query first, then delete, so need Transactional
-    // annotation
-    @Transactional
-    void deleteByInstanceName(String instanceName);
+  // Will issue select query first, then delete, so need Transactional
+  // annotation
+  @Transactional void deleteByInstanceName(String instanceName);
 }
