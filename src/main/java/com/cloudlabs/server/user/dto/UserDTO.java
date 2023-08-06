712e9570433dafbf2257ed6747ce02e278d3d715
DancinParrot <kaizhetan@yahoo.com>
2023-08-06 16:30:42 +0800

    fix: fix merge error resulting in fail compile

--- src/main/java/com/cloudlabs/server/user/dto/UserDTO.java
+++ src/main/java/com/cloudlabs/server/user/dto/UserDTO.java
@@ -1,25 +1,47 @@
 package com.cloudlabs.server.user.dto;
 
+import com.cloudlabs.server.role.dto.RoleDTO;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.List;
 
 @JsonInclude(Include.NON_DEFAULT)
 public class UserDTO {
-    
-    private String fullName;
+
+    private String fullname;
     private String username;
     private String email;
     private String currentPassword;
     private String newPassword;
 
-    public UserDTO() {}
+    private List<RoleDTO> roles;
 
-    public String getFullName() {
-        return this.fullName;
+    public UserDTO() {
     }
 
-    public void setFullName(String fullname) {
-        this.fullName = fullname;
+    public UserDTO(String fullname, String username, String email,
+            String currentPassword) {
+        this.fullname = fullname;
+        this.username = username;
+        this.email = email;
+        this.currentPassword = currentPassword;
+    }
+
+    public UserDTO(String fullname, String username, String email,
+            String currentPassword, List<RoleDTO> roles) {
+        this.fullname = fullname;
+        this.username = username;
+        this.email = email;
+        this.currentPassword = currentPassword;
+        this.roles = roles;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
     }
 
     public String getUsername() {
@@ -41,7 +63,7 @@ public class UserDTO {
     public String getCurrentPassword() {
         return this.currentPassword;
     }
-    
+
     public void setCurrentPassword(String current_password) {
         this.currentPassword = current_password;
     }
@@ -53,5 +75,12 @@ public class UserDTO {
     public void setNewPassword(String new_password) {
         this.newPassword = new_password;
     }
-    
+
+    public List<RoleDTO> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<RoleDTO> roles) {
+        this.roles = roles;
+    }
 }
